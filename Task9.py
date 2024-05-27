40635847c9c13187c08f7ca355c588480f1fbcbe
Jananisoundar <jananisoundar12@gmail.com>
2024-05-27 21:23:39 +0530

    Add files via upload

---
+++ Task9.py
@@ -0,0 +1,7 @@
+# what is the following output for python code
+# data = [10,501,22,37,100,999,87,351]
+# result = filter (lambda x:x>4, data)
+# print(list(result))
+
+
+# Output for the above code: [10, 501, 22, 37, 100, 999, 87, 351]
